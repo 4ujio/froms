25308053f401104d9d5d4d256fc99deaf212847d
4ujio <4ujio@users.noreply.github.com>
2023-09-18 11:29:51 +0000

    deploy: 1230fa8769d1c7152000824cd0e2754277378947

---
+++ js/168.aa85e44c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmymihua"]=self["webpackChunkmymihua"]||[]).push([[168],{1168:function(u,n,t){t.r(n),t.d(n,{default:function(){return r}});var s=function(){var u=this;u._self._c;return u._m(0)},a=[function(){var u=this,n=u._self._c;return n("div",{staticClass:"about"},[n("h1",[u._v("This is an about page")])])}],e=t(1001),i={},l=(0,e.Z)(i,s,a,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=168.aa85e44c.js.map
